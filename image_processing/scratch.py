14b17c288238a6ff09223b98a1bf9b8a53098180
Jun-Sang Park <jp118@cornell.edu>
2014-12-13 15:22:27 -0600

    scratch pad for batch correction

--- image_processing/scratch.py
+++ image_processing/scratch.py
@@ -1,28 +1,52 @@
+import os
 import wx
 
-class MainWindow(wx.Frame):
+
+class BatchCorrectionWindow(wx.Frame):
     def __init__(self, parent, title):
         wx.Frame.__init__(self, parent, title=title, size=(200,100))
         self.control = wx.TextCtrl(self, style=wx.TE_MULTILINE)
-        self.CreateStatusBar() # A Statusbar in the bottom of the window
+        self.CreateStatusBar() # A StatusBar in the bottom of the window
 
         # Setting up the menu.
         filemenu= wx.Menu()
 
-        # wx.ID_ABOUT and wx.ID_EXIT are standard IDs provided by wxWidgets.
-        filemenu.Append(wx.ID_ABOUT, "&About"," Information about this program")
-        filemenu.AppendSeparator()
-        filemenu.Append(wx.ID_EXIT,"E&xit"," Terminate the program")
+        # wx.ID_ABOUT and wx.ID_EXIT are standard ids provided by wxWidgets.
+        menuAbout = filemenu.Append(wx.ID_ABOUT, '&About',' Information about this program')
+        menuExit = filemenu.Append(wx.ID_EXIT,'E&xit',' Terminate the program')
 
         # Creating the menubar.
         menuBar = wx.MenuBar()
-        menuBar.Append(filemenu,"&File") # Adding the "filemenu" to the MenuBar
+        menuBar.Append(filemenu,'&File') # Adding the 'filemenu' to the MenuBar
         self.SetMenuBar(menuBar)  # Adding the MenuBar to the Frame content.
+
+        # Set events.
+        self.Bind(wx.EVT_MENU, self.OnAbout, menuAbout)
+        self.Bind(wx.EVT_MENU, self.OnExit, menuExit)
+
         self.Show(True)
 
-        menuItem = filemenu.Append(wx.ID_ABOUT, "&About"," Information about this program")
-        self.Bind(wx.EVT_MENU, self.OnAbout, menuItem)
+    def OnAbout(self,e):
+        # A message dialog box with an OK button. wx.OK is a standard ID in wxWidgets.
+        dlg = wx.MessageDialog( self, 'A small text editor', 'About Sample Editor', wx.OK)
+        dlg.ShowModal() # Show it
+        dlg.Destroy() # finally destroy it when finished.
 
+    def OnExit(self,e):
+        self.Close(True)  # Close the frame.
+
+    def OnOpen(self,e):
+        ''' Open a file'''
+        self.dirname = ''
+        dlg = wx.FileDialog(self, 'Choose a file', self.dirname, '', '*.*', wx.OPEN)
+        if dlg.ShowModal() == wx.ID_OK:
+            self.filename = dlg.GetFilename()
+            self.dirname = dlg.GetDirectory()
+            f = open(os.path.join(self.dirname, self.filename), 'r')
+            self.control.SetValue(f.read())
+            f.close()
+        dlg.Destroy()
+        
 app = wx.App(False)
-frame = MainWindow(None, "Sample editor")
+frame = BatchCorrectionWindow(None, 'Batch correction ')
 app.MainLoop()
