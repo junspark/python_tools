9699dededddf8c615c3fb0356db4323eec4f5f45
junspark <jun.sang.park@outlook.com>
2026-08-26 14:49:04 -0500

    Rename lens3C/lens4C to C Lens Stack 3/4
    Matches the existing C Lens Stack 1/2/7 numbering convention.

--- ops/pv_logger/pv_logger_gui.py
+++ ops/pv_logger/pv_logger_gui.py
@@ -219,7 +219,7 @@ DEVICE_CATEGORIES = [
         "L1", "L2", "RL", "CRL1", "CRL2", "lens4B", "lens5B",
     ]),
     ("C Lens Stacks", [
-        "C Lens Stack 1", "C Lens Stack 2", "C Lens Stack 7", "lens3C", "lens4C",
+        "C Lens Stack 1", "C Lens Stack 2", "C Lens Stack 3", "C Lens Stack 4", "C Lens Stack 7",
     ]),
     ("D Lens Stacks", [
         "D Lens Stack 1", "D Lens Stack 2", "D Lens Stack 3", "D Lens Stack 4",
